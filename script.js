3b070d11e4f4619d9136c2d7c6804b94ba1b0600
infomoneymovesacademy-cpu <info.moneymovesacademy@gmail.com>
2025-12-12 16:35:42 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
+
+registerBtn.addEventListener('click', () =>{
+  container.classList.add("active");
+});
+
+loginBtn.addEventListener('click', () =>
+  {
+  container.classList.remove("active");
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  const button = document.getElementById("contact");
+
+  button.addEventListener("click", function() {
+    //Open the other HTML file in a new tab
+    window.open("contact.html", "_blank");
+  });
+});
